2d9ec6fc6bfac97baa79386d3f199d106e86abeb
c03rcion <areeves9@icloud.com>
2020-04-29 12:19:49 -0700

    config package static folders

--- chemovar/terpenes/forms.py
+++ chemovar/terpenes/forms.py
@@ -8,6 +8,22 @@ from wtforms.validators import DataRequired
 
 
 class TerpeneForm(FlaskForm):
-    aroma = StringField('aroma', validators=[DataRequired()])
+    aroma = SelectField(
+         'aroma',
+         choices=[
+             ('Sweet', 'Sweet'),
+             ('Woody', 'Woody'),
+             ('Citrus', 'Citrus'),
+             ('Pepper', 'Pepper'),
+             ('Herbal', 'Herbal'),
+             ('Pine', 'Pine'),
+             ('Rosy', 'Rosy'),
+             ('Floral', 'Floral'),
+             ('Earthy', 'Earthy'),
+             ('Fruity', 'Fruity'),
+             ('Musky', 'Musky'),
+             ('Skunky', 'Skunky'),
+            ]
+     )
     compound_id = SelectField(coerce=int)
     submit = SubmitField()
